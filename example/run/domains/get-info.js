24c53fd31ea493ccce3cfdd78efa837ccd69042b
Anton <anton@adc.sh>
2019-07-31 18:31:26 +0300

    get-info wiki

--- example/run/domains/get-info.js
+++ example/run/domains/get-info.js
@@ -1,7 +1,7 @@
 import bosom from 'bosom'
 import NameCheap from '../../../src'
-import Create from '../../api/domains/create'
-import GetInfo from '../../api/domains/get-info'
+// import Create from '../../api/domains/create'
+// import GetInfo from '../../api/domains/get-info'
 
 (async () => {
   try {
@@ -9,11 +9,14 @@ import GetInfo from '../../api/domains/get-info'
     const namecheap = new NameCheap({
       user, key, sandbox: true, ip,
     })
-    const d = new Date().toLocaleString().replace(/[ :]/g, '-')
-    const domain = `rqt-example-${d}.com`
-    await Create(domain, namecheap)
-    const res = await GetInfo(domain, namecheap)
+    // const d = new Date().toLocaleString().replace(/[ :]/g, '-')
+    // const domain = `rqt-example-${d}.com`
+    // await Create(domain, namecheap)
+    // const res = // await GetInfo(domain, namecheap)
+    /* start example */
+    const res = await namecheap.domains.getInfo('node-test.app')
     console.log(res)
+    /* end example */
   } catch ({ message }) {
     console.log(message)
   }
--- wiki/Domains/3-get-info.md
+++ wiki/Domains/3-get-info.md
@@ -6,9 +6,15 @@
 
 Returns information about the requested domain.
 
-<typedef narrow flatten>types/api/domains/get-info.xml</typedef>
+<typedef flatten details="DomainDetails,Whoisguard,EmailDetails,PremiumDnsSubscription,DnsDetails,ModificationRights">
+  types/api/domains/get-info.xml
+</typedef>
 
-%EXAMPLE: example/api/domains/get-info%
+---
+
+_The [example](../blob/master/example/run/domains/get-info.js) shows how to retrieve information about a registered domain on the account._
+
+%EXAMPLE: example/run/domains/get-info%
 %FORK-js example/run/domains/get-info%
 
 %~ width="25"%
